9859c6697320cb85ab89b11f08744722ab0cf642
YasserS-21 <saadi.yasser04@gmail.com>
2024-05-13 05:08:31 -0400

    set up health check

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const usersController = require("./controllers/usersController");
+const itemsController = require("./controllers/itemsController");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (request, response) => {
+    response.status(200).json({ message: 'Service is running'})
+});
+
+// app.use("/users", usersController)
+
+// app.use("/items", itemsController);
+
+module.exports = app;
