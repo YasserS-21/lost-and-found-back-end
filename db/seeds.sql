3efe7cba3b0ba53ab5cfb070286d1ce560bd9eb5
YasserS-21 <saadi.yasser04@gmail.com>
2024-05-13 05:09:50 -0400

    create dummy data for items table and users table

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO users (id, first_name, last_name, email, pic)
+  VALUES
+  (1, 'Yadel', 'Ingaberg', 'iorton0@imdb.com', 'https://storage.googleapis.com/hatchways-app.appspot.com/assessments/data/frontend/images/voluptasdictablanditiis.jpg'), 
+  (2, 'Avamm',  'Clarke', 'cboards1@weibo.com', 'https://storage.googleapis.com/hatchways-app.appspot.com/assessments/data/frontend/images/voluptasautreprehenderit.jpg'), 
+  (3, 'Skalith', 'Laurens', 'lromanet2@wired.com', 'https://storage.googleapis.com/hatchways-app.appspot.com/assessments/data/frontend/images/aspernaturnonsapiente.jpg'), 
+  (4, 'Mybuzz', 'Berti', 'bskitt3@aboutads.info', 'https://storage.googleapis.com/hatchways-app.appspot.com/assessments/data/frontend/images/autautdeserunt.jpg')
+  ;
+
+INSERT INTO items (id, name, location, description, date_lost, image_url, owner_id)
+  VALUES
+     (1, 'Yellow Shirt', 'Studio 1', 'kinda orangish yellow', '2024-05-12', '', 3), 
+  (2, 'Star Braclet',  'Studio 1', 'cboards1@weibo.com', '2024-05-12', '', 2), 
+  (3, 'Dress', 'Studio 2', 'lromanet2@wired.com', '2024-05-12', '', 1), 
+  (4, 'Yoga Mat', 'Studio 1', 'bskitt3@aboutads.info', '2024-05-12', '', 1)
+  ;
+
+
