dde06a9b44009012836cadbd21c99c23b4d266e9
YasserS <56772311+YasserS-21@users.noreply.github.com>
2024-05-28 13:42:02 -0400

    Queries and Controller for Items
    GET, POST, PUT , DELETE requests for items
    (No Validation)

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -1,11 +1,37 @@
 const { Router } = require('express'); 
-const { getAllItems } = require('../queries/itemsQueries');
+const { getAllItems, getItemById, createItem, updateItem, deleteItem } = require('../queries/itemsQueries');
 
 const itemsController = Router();
 
 itemsController.get("/", async (request, response) => {
     const items = await getAllItems();
     response.status(200).json({ data: items });
-  });
+});
 
-  module.exports = itemsController;
+itemsController.get("/:id", async (request, response) => {
+    const { id } = request.params;
+    const item = await getItemById(Number(id));
+    response.status(200).json({ data: item });
+  });
+  
+itemsController.post("/", async (request,response) => {
+  const item = request.body;
+  const createdItem = await createItem(item);
+  response.status(200).json({data: createdItem})
+})
+
+itemsController.put("/:id", async (request,response) => {
+  const {id} = request.params;
+  const item = request.body;
+  const updatedItem = await updateItem(Number(id), item);
+  response.status(200).json({data: updatedItem})
+})
+
+itemsController.delete("/:id", async (request,response) => {  
+  const {id} = request.params;
+  const deletedItem = await deleteItem(Number(id));
+  response.status(200).json({data: deletedItem})
+})
+
+
+module.exports = itemsController;
