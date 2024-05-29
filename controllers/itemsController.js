57496ccfeed03fc413e8ef38843ac3396d83d557
YasserS <56772311+YasserS-21@users.noreply.github.com>
2024-05-29 00:09:35 -0400

    Data Validation and Error Handling for Items
    Add Validations and Error Handling for
    GET, PUT, DELETE, POST requests
    
    Validate Item Object
    Validate ID

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -1,37 +1,120 @@
-const { Router } = require('express'); 
-const { getAllItems, getItemById, createItem, updateItem, deleteItem } = require('../queries/itemsQueries');
+const { Router } = require("express");
+const {
+  getAllItems,
+  getItemById,
+  createItem,
+  updateItem,
+  deleteItem,
+} = require("../queries/itemsQueries");
+const {
+  validateId,
+  validateItem,
+} = require("./controllerHelpers/itemControllerHelper.js");
 
 const itemsController = Router();
 
 itemsController.get("/", async (request, response) => {
+  try {
     const items = await getAllItems();
     response.status(200).json({ data: items });
+  } catch (err) {
+    response.status(500).json({ error: err.message });
+  }
 });
 
 itemsController.get("/:id", async (request, response) => {
+  try {
     const { id } = request.params;
-    const item = await getItemById(Number(id));
-    response.status(200).json({ data: item });
-  });
-  
-itemsController.post("/", async (request,response) => {
-  const item = request.body;
-  const createdItem = await createItem(item);
-  response.status(200).json({data: createdItem})
-})
+    
+    // Check if the id is valid
+    if (!validateId(Number(id))) {
+      return response.status(404).json({ error: "Invalid id" });
+    }
 
-itemsController.put("/:id", async (request,response) => {
-  const {id} = request.params;
-  const item = request.body;
-  const updatedItem = await updateItem(Number(id), item);
-  response.status(200).json({data: updatedItem})
-})
+    // This will return the all items that have the specified id (we should only have one item)
+    const item = await getItemById(Number(id))
 
-itemsController.delete("/:id", async (request,response) => {  
-  const {id} = request.params;
-  const deletedItem = await deleteItem(Number(id));
-  response.status(200).json({data: deletedItem})
-})
+    // Check if the item exists
+    if (!item.length) {
+      return response.status(404).json({ error: "Item not found" });
+    }
 
+    // send the only first item which should be the only item
+    response.status(200).json({ data: item[0] });
+
+  } catch (err) {
+    response.status(500).json({ error: err.message });
+  }
+});
+
+itemsController.post("/", async (request, response) => {
+  try {
+    const item = request.body;
+
+    // Check if the item is valid
+    if (!validateItem(item)) {
+      return response.status(400).json({ error: "Invalid item" });
+    }
+
+    // create the item
+    const createdItem = await createItem(item);
+
+    response.status(201).json({ data: createdItem });
+  } catch (err) {
+    response.status(500).json({ error: err.message });
+  }
+});
+
+itemsController.put("/:id", async (request, response) => {
+  try {
+
+    const { id } = request.params;
+
+    // validate the request id
+    if (!validateId(Number(id))) {
+      return response.status(404).json({ error: "Invalid id" });
+    }
+    
+    const item = request.body;
+
+    // validate the update request
+    if (!validateItem(item)) {
+      return response.status(400).json({ error: "Invalid item" });
+    }
+
+    // This will return the all items that have the specified id (we should only have one item)
+    const updatedItem = await updateItem(Number(id), item);
+
+    // If the item has not been updated then the item does not exist with that id
+    if (!updatedItem.length) {
+      return response.status(404).json({ error: "Item not found" });
+    }
+
+    response.status(200).json({ data: updatedItem[0] });
+  } catch (err) {
+    response.status(500).json({ error: err.message });
+  }
+});
+
+itemsController.delete("/:id", async (request, response) => {
+  try {
+
+    const { id } = request.params;
+
+    // Check if the id is valid
+    if (!validateId(Number(id))) {
+      return response.status(404).json({ error: "Invalid id" });
+    }
+    // This will return the all items that have the specified id (we should only have one item)
+    const deletedItem = await deleteItem(Number(id));
+    // If the item has not been deleted then the item does not exist with that id
+    if (!deletedItem.length) {
+      return response.status(404).json({ error: "Item not found" });
+    }
+    response.status(200).json({ data: deletedItem });
+  } catch (err) {
+    response.status(500).json({ error: err.message });
+  }
+});
 
 module.exports = itemsController;
--- queries/itemsQueries.js
+++ queries/itemsQueries.js
@@ -7,22 +7,22 @@ const getAllItems = async () => {
 }
 
 const getItemById = async (id) => {
-    const response = await db.one("SELECT * FROM items WHERE id = $1", [id]);
+    const response = await db.any("SELECT * FROM items WHERE id = $1", [id]);
     return response;
 }
 
 const createItem = async (item) => {
-    const { id, name, description, location, date_lost } = item;
-    const response = await db.one("INSERT INTO items (id, name, description, location, date_lost) VALUES ($1, $2, $3, $4, $5) RETURNING *", [id, name, description, location, date_lost]);
+    const {name, description, location, date_lost } = item;
+    const response = await db.one("INSERT INTO items (name, description, location, date_lost) VALUES ($1, $2, $3, $4) RETURNING *", [name, description, location, date_lost]);
     return response;
 }
 const updateItem = async (id, item) => {
     const { name, description, location, date_lost } = item;
-    const response = await db.one("UPDATE items SET name = $1 , description = $2 , location = $3 , date_lost= $4 WHERE id = $5 RETURNING *", [name, description, location, date_lost, id])
+    const response = await db.any("UPDATE items SET name = $1 , description = $2 , location = $3 , date_lost= $4 WHERE id = $5 RETURNING *", [name, description, location, date_lost, id])
     return response;
 }
 const deleteItem = async (id) => {
-    const response = await db.one("DELETE FROM items WHERE id = $1 RETURNING *", [id]);
+    const response = await db.any("DELETE FROM items WHERE id = $1 RETURNING *", [id]);
     return response
 }
 
